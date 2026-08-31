708afd6f74cd07047b175a85a03d0714769c9ae1
Lucas Alencar <lucasalencarxisto-stack>
2026-08-31 11:20:51 -0300

    feat: create initial home screen

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,18 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
 
-import { HomePage } from './home.page';
-
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
-
-  beforeEach(async () => {
-    fixture = TestBed.createComponent(HomePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonContent],
+})
+export class HomePage {}
