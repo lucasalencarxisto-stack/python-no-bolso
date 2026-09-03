4b4adc4563ca7c54fb0189d9111636ffc97882c4
Lucas Alencar <lucasalencarxisto-stack>
2026-09-03 06:54:54 -0300

    fix: corrige configuração da home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonContent],
 })
-export class HomePage {
-  constructor() {}
-}
+export class HomePage {}
