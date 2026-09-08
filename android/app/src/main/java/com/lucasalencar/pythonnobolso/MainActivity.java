ffa3d5009b5d2c4daa6d5290cd992c7caca56fb3
Lucas Alencar <lucasalencarxisto-stack>
2026-09-08 14:38:00 -0300

    fix: define identificador oficial do app Android

---
+++ android/app/src/main/java/com/lucasalencar/pythonnobolso/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lucasalencar.pythonnobolso;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'python-no-bolso',
+  appId: 'com.lucasalencar.pythonnobolso',
+  appName: 'Python no Bolso',
   webDir: 'www'
 };
 
